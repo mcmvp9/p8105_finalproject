4c8b43e3049095305a5c51813d155a09926b3d6b
“John <mbc2178@cumc.columbia.edu>
2022-12-09 18:46:54 -0500

    shiny app updates

--- shiny-appp/app.R
+++ shiny-appp/app.R
@@ -9,59 +9,71 @@ library(plotly)
 library(viridis)
 library(rsconnect)
 
+# Import Dataset
 world_cup = read_csv("./12_4_dataset.csv") %>%
-  janitor::clean_names() %>%
-  select(country,w, pld, d, rank, gf, ga) 
-country = world_cup %>% distinct(country) %>% pull()
-min_w = world_cup %>% distinct(w) %>% min()
-min_pld = world_cup %>% distinct(pld) %>% min()
-min_d = world_cup %>% distinct(d) %>% min()
-min_gf = world_cup %>% distinct(gf) %>% min()
-min_ga = world_cup %>% distinct(ga) %>% min()
+   janitor::clean_names() %>%
+         select(country,w, pld, d, rank, gf, ga) 
 
-max_w = world_cup %>% distinct(w) %>% max()
-max_pld = world_cup %>% distinct(pld) %>% max()
-max_d = world_cup %>% distinct(d) %>% max()
-max_gf = world_cup %>% distinct(gf) %>% max()
-max_ga = world_cup %>% distinct(ga) %>% max()
-# Define UI for application that draws a histogram
+# 
+country = world_cup %>% distinct(country) %>% pull()
+
+#  min_w = world_cup %>% distinct(w) %>% min()
+ # min_pld = world_cup %>% distinct(pld) %>% min()
+ # min_d = world_cup %>% distinct(d) %>% min()
+ # min_gf = world_cup %>% distinct(gf) %>% min()
+ # min_ga = world_cup %>% distinct(ga) %>% min()
+
+ # max_w = world_cup %>% distinct(w) %>% max()
+ # max_pld = world_cup %>% distinct(pld) %>% max()
+ # max_d = world_cup %>% distinct(d) %>% max()
+ # max_gf = world_cup %>% distinct(gf) %>% max()
+ # max_ga = world_cup %>% distinct(ga) %>% max()
+  
+# Define UI for a
 ui <- fluidPage(
-  titlePanel("Movie explorer"),
+  titlePanel("2022 FIFA WORLD CUP PREDICTION TOOL"),
   fluidRow(
     column(3,
            wellPanel(
              h4("Filter"),
              selectInput(
                "country_choice", 
-               label = h3("Select country"),
+               label = h3("Select Country"),
                choices = country),
              
              # sliderInput widget for pld
              sliderInput(
                "number_games_range", 
-               label = h3("Choose number games range"), 
-               min = 0, max = max_pld, value = c(113)),
+               label = h3("Choose Number of Matches Played"), 
+               min = 0, max = 7, value = c(7)),
              
              # sliderInput widget for d 
              sliderInput(
                "draw_games_range", 
                label = h3("Choose draw games range"), 
-               min = min_d, max = max_d, value = c(22)),
+               min = 0, max = 7, value = c(2)),
              
              # sliderInput widget for gf
              sliderInput(
                "goals_for_range", 
                label = h3("Choose goals for range"), 
-               min = min_gf, max = max_gf, value = c(236)),
+               min = 0, max = 20, value = c(10)),
              
              # sliderInput widget for ga 
              sliderInput(
                "goals_against_range", 
                label = h3("Choose goals against range"), 
-               min = min_ga, max = max_ga, value = c(130)),
+               min = 0, max = 15, value = c(0)),
            )
     ),
+    
+      </center>
+      <img 
+      src="images/resume_pic.png"style="width:25%">
+      
     mainPanel(
+      img(src='qatar.png', align = "right"),
+      
             tableOutput("table")
           )
     
@@ -100,10 +112,24 @@ server <- function(input, output) {
     world_cup %>%
       filter(
         country == input$country_choice) %>%
+      
       mutate(pld = input$number_games_range[1],
-             prediction = -1.56 + 0.660*pld - 0.628*d + 0.0159*rank + 0.154*gf - 0.224*ga) %>% 
+             d = input$draw_games_range[1],
+             gf = input$goals_for_range[1],
+             ga = input$goals_against_range[1],
+          
+  prediction = -1.56 + 0.660*pld - 0.628*d + 0.0159*rank + 0.154*gf - 0.224*ga) %>% 
+    
       select(-w) %>% 
-      rename("Predicted Games Won"=prediction)
+      
+        rename( "Country" = country,
+                "Matches Played" = pld,
+                "Matches Drawn" = d,
+                "FIFA Rankings 2022" = rank,
+                "Goals Scored" = gf,
+                "Goals Against" = ga,
+               "Predicted Games Won" = prediction
+               )
   })
 }
 
